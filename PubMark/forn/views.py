668abe58fbd11592a0b9d90b6e848c2829a579b6
Armand <oliveira.armand@poli.ufrj.br>
2018-08-06 16:32:55 -0300

    implementação API de dados

--- PubMark/forn/views.py
+++ PubMark/forn/views.py
@@ -1,5 +1,27 @@
 from django.http import HttpResponse
 
+from .models import Fornecedor
+import pandas as pd
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request,CNPJ):
+    fornecedor = Fornecedor.objects.get(cnpj = CNPJ)
+    atributos_fornecedor = vars(fornecedor)
+    return HttpResponse(str(atributos_fornecedor))
+
+def indicadores(request,CNPJ):
+    fornecedor = Fornecedor.objects.get(cnpj = CNPJ)
+    indicadores = fornecedor.indicadores()
+    return HttpResponse(str(indicadores))
+
+def rivais(request,CNPJ):
+    fornecedor = Fornecedor.objects.get(cnpj = CNPJ)
+    rivais = fornecedor.indicadores_rivais()
+    return HttpResponse(str(rivais))
+
+
+
+
