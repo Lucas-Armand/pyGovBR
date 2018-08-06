5be071021613366919b554d4e26a1b68ea3032a4
Armand <oliveira.armand@poli.ufrj.br>
2018-08-06 16:33:11 -0300

    implementação API de dados

--- PubMark/forn/urls.py
+++ PubMark/forn/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /forn/00097117000135
+    path('<str:CNPJ>/', views.detail, name='detail'),
+    path('<str:CNPJ>/indicadores', views.indicadores, name='indicadores'),
+    path('<str:CNPJ>/rivais', views.rivais, name='rivais'),
+
 ]
