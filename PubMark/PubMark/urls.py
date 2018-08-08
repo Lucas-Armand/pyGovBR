ce363744fb287a76854717d3af44fc03bbf07dde
Armand <oliveira.armand@poli.ufrj.br>
2018-08-08 17:01:19 -0300

    adicionando dashboard

--- PubMark/PubMark/urls.py
+++ PubMark/PubMark/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('dashboard/', include('dashboard.urls')),
     path('forn/', include('forn.urls')),
     path('admin/', admin.site.urls),
 ]
